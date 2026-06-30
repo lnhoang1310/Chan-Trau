24b2217a6a4c04edb125d30ded9fdb70b1d36fd3
lnhoang1310 <hoang18102004@gmail.com>
2026-06-30 17:21:51 +0700

    add param

--- chan_trau_gateway/main/chan_trau_gateway.c
+++ chan_trau_gateway/main/chan_trau_gateway.c
@@ -15,18 +15,6 @@ nmea_msg gpsx;
 
 void app_main(void)
 {
-    uint8_t buf[256];
-    gpio_config_t io_conf = {
-        .intr_type = GPIO_INTR_DISABLE,
-        .mode = GPIO_MODE_OUTPUT,
-        .pin_bit_mask = (1ULL << 2),
-        .pull_down_en = 0,
-        .pull_up_en = 0
-    };
-    gpio_config(&io_conf);
-    gpio_set_level(2, 0);
-    vTaskDelay(pdMS_TO_TICKS(1000));
-    gpio_set_level(2, 1);
     // if (!sim_init())
     // {
     //     ESP_LOGE(TAG, "SIM Init Failed\n");
@@ -44,34 +32,10 @@ void app_main(void)
         return;
     }
 
-    // NMEA_GNRMC_Init();
-    lora_set_preamble_length(3);
-    lora_set_frequency(868E6);
-    lora_set_spreading_factor(7);
-    lora_set_bandwidth(125E3);
-    lora_set_coding_rate(5);
-    lora_set_sync_word(0x12);
+    NMEA_GNRMC_Init();
 
     while (1)
     {
-        char msg[] = "ESP32 LoRa";
-        lora_send_packet((uint8_t*)msg, strlen(msg));
-        ESP_LOGI(TAG, "Send OK\n");
-        gpio_set_level(2, (gpio_get_level(2) == 1) ? 0 : 1);
         vTaskDelay(pdMS_TO_TICKS(1000));
-
-        // lora_receive();
-        // if(lora_received())
-        // {
-        //     ESP_LOGI(TAG, "Packet received\n");
-        // }
-        // int len = lora_receive_packet(buf, sizeof(buf)-1);
-        // if(len > 0)
-        // {
-        //     buf[len] = '\0';
-        //     printf("Recv: %s\n", buf);
-        //     gpio_set_level(2, gpio_get_level(2) ^ 1);
-        // }
-        // vTaskDelay(pdMS_TO_TICKS(10));
     }
 }
